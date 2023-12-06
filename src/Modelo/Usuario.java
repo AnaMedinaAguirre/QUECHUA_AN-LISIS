80365240b2dcd3776594fd583ad67ff5560dbbc7
AnaMedinaAguirre <144259380+AnaMedinaAguirre@users.noreply.github.com>
2023-12-06 09:30:29 -0500

    Update Usuario.java

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
-/**
- *
- * @author ANITA
- */
 public class Usuario {
+    private String nombre;
+    private String email;
+    private int puntos;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
+    public void SumarPuntos(int respuestas){
+        this.puntos += 5*respuestas;
+    }
+
 }
